c0c30672c64dda2be81bd18ce75375d0a5e5887e
Martina Lumastro <martinalumastro99@gmail.com>
2024-07-16 18:18:17 +0200

    palindroma

--- js/js-palindroma.js
+++ js/js-palindroma.js
@@ -2,4 +2,27 @@ console.log('palindroma')
 
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
-let parolaUtente = prompt('Inserisci una parola')
+let userParola = prompt('Inserisci una parola')
+let userParolaNeutra = userParola.toLowerCase()
+
+function palindroma(parola) {
+    let parolaLength = parola.length
+    let result
+
+    for(let i = 0; i < parolaLength; i++) {
+        if (parola[i] === parola[(parolaLength - 1) - i]) {
+            result = true
+        } else {
+            result = false
+        }
+    }
+    return result
+}
+
+let verificaPalindroma = palindroma(userParolaNeutra)
+
+if(verificaPalindroma) {
+    console.log('La parola "' + userParola + '" è palindroma')
+} else {
+    console.log('La parola "' + userParola + '" non è palindroma')
+}
