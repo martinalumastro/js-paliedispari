ac52c4971ffc212390de54aa9d9d72713c0d7e97
Martina Lumastro <martinalumastro99@gmail.com>
2024-07-16 19:28:28 +0200

    Palindroma versione finale

--- js/js-palindroma.js
+++ js/js-palindroma.js
@@ -2,25 +2,41 @@ console.log('palindroma')
 
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
-let userParola = prompt('Inserisci una parola')
-let userParolaNeutra = userParola.toLowerCase()
+// let userParola = prompt('Inserisci una parola')
+// let userParolaNeutra = userParola.toLowerCase()
 
-function palindroma(parola) {
-    let parolaLength = parola.length
-    let result
+// function palindroma(parola) {
+//     let parolaLength = parola.length
+//     let result
 
-    for(let i = 0; i < Math.floor(parolaLength /2); i++) {
-        if (parola[i] !== parola[(parolaLength - 1) - i]) {
-            return false
-        }
-        return true
-    }
-}
+//     for(let i = 0; i < Math.floor(parolaLength /2); i++) {
+//         if (parola[i] !== parola[(parolaLength - 1) - i]) {
+//             return false
+//         }
+//         return true
+//     }
+// }
 
-let verificaPalindroma = palindroma(userParolaNeutra)
+// let verificaPalindroma = palindroma(userParolaNeutra)
 
-if(verificaPalindroma) {
-    console.log('La parola "' + userParola + '" è palindroma')
+// if(verificaPalindroma) {
+//     console.log('La parola "' + userParola + '" è palindroma')
+// } else {
+//     console.log('La parola "' + userParola + '" non è palindroma')
+// }
+
+//SECONDA PROVA DELL'ESERCIZIO
+let parola = prompt('Inserisci una parola');
+
+//singoli caratteri parola
+let caratteri = parola.split('');
+
+//parola invertita
+let inverso = caratteri.reverse().join('');
+
+//verifica
+if(parola === inverso) {
+    console.log(parola + ' è una parola palindroma')
 } else {
-    console.log('La parola "' + userParola + '" non è palindroma')
+    console.log(parola + ' non è una parola palindroma')
 }
